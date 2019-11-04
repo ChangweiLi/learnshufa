9a6b421591f27f4dff716a32105aa5e8b1ae6ef1
lichangwei <1369990551@qq.com>
2019-11-04 20:42:32 +0800

    init

---
+++ src/main/java/com/scau/learnshufa/LearnshufaApplication.java
@@ -0,0 +1,13 @@
+package com.scau.learnshufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnshufaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnshufaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scau/learnshufa/LearnshufaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scau.learnshufa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnshufaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
