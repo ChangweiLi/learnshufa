e89b75519ee83416348212e83773c5a79af7bb19
lichangwei <1369990551@qq.com>
2019-11-08 23:17:19 +0800

    init

--- src/main/java/com/scau/learnshufa/controller/IndexController.java
+++ src/main/java/com/scau/learnshufa/controller/IndexController.java
@@ -2,13 +2,43 @@ package com.scau.learnshufa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/shufa")
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String index(){
-        return "hello world";
+        System.out.println("this is index");
+        return "index";
     }
+
+    /*
+    英文书法教程页面
+     */
+    @RequestMapping("/index/english")
+    public String english(){
+        return "english";
+    }
+
+    /*
+    名人书法欣赏
+     */
+    @RequestMapping("/index/famous")
+    public String famous(){
+        return "famous";
+    }
+
+    /*
+    根据字体来筛选或者查询教程
+     */
+    @RequestMapping("/index/font")
+    public String font(){
+        return "font";
+    }
+
+
+
 
 }
